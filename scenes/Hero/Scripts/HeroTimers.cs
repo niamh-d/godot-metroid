c5e121a0735a919e4f4909c2cd8021373d99bbe0
Niamh <niamh.linguist@gmail.com>
2025-06-21 12:40:38 +0300

    slide timers

---
+++ scenes/Hero/Scripts/HeroTimers.cs
@@ -0,0 +1,43 @@
+using Godot;
+
+public class HeroTimers
+{
+    private HeroStateMachine Hero;
+    public Timer SlideTimer;
+    public Timer SlideStandUpTimer;
+    private bool TimersInitialized;
+
+    public HeroTimers(HeroStateMachine hero, ref bool initOk)
+    {
+        Hero = hero;
+        initOk = InitHeroTimers();
+    }
+
+    private bool InitHeroTimers()
+    {
+        TimersInitialized = true;
+
+        SlideTimer = GetTimerNode("SlideTimer");
+        if (!TimersInitialized) return false;
+
+        SlideStandUpTimer = GetTimerNode("SlideStandUpTimer");
+        if (!TimersInitialized) return false;
+
+        return true;
+    }
+
+    private Timer GetTimerNode(string timerNode)
+    {
+        string timerPath = "./Timers/" + timerNode;
+        var timer = Hero.GetNode<Timer>(timerPath);
+
+        if (timer == null)
+        {
+            TimersInitialized = false;
+            GD.PrintErr("Error in HeroTimers.cs – GetTimerNode() – Could not initialize timer, node: '" + timerNode + "' not found!");
+        }
+
+        return timer;
+    }
+
+}
