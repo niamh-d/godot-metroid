ff3b8273872cf83a4fdcb4b9970aa8c6764837c2
Niamh <niamh.linguist@gmail.com>
2025-06-21 12:24:56 +0300

    small and large jump

--- scenes/Hero/Scripts/HeroStateInitJump.cs
+++ scenes/Hero/Scripts/HeroStateInitJump.cs
@@ -3,7 +3,6 @@ using Godot;
 public class HeroStateInitJump : IHeroState
 {
     private float JumpForce = -700.0f;
-    private bool JumpInitiated = false;
 
     public IHeroState DoState(HeroStateMachine hero, float delta)
     {
@@ -14,20 +13,11 @@ public class HeroStateInitJump : IHeroState
     {
         hero.DisableSnap();
 
-        if (!JumpInitiated)
-        {
-            JumpInitiated = true;
-            hero.HeroMoveLogic.Velocity.y = JumpForce;
-        }
+        hero.HeroMoveLogic.Velocity.y = JumpForce;
+
 
         hero.HeroAnimations.Play("HeroInitJump");
 
-        if (hero.LastPlayedHeroAnimation.Equals("HeroInitJump"))
-        {
-            this.JumpInitiated = false;
-            return hero.StateJump;
-        }
-
-        return hero.StateInitJump;
+        return hero.StateJump;
     }
 }
--- scenes/Hero/Scripts/HeroStateJump.cs
+++ scenes/Hero/Scripts/HeroStateJump.cs
@@ -1,6 +1,11 @@
 
+using Godot;
+
 public class HeroStateJump : IHeroState
 {
+    private const float CutJumpThreshold = -200.0f;
+    private const float JumpForceAfterJumpCutShort = -320.0f;
+
     public IHeroState DoState(HeroStateMachine hero, float delta)
     {
         return Jump(hero, delta);
@@ -11,6 +16,12 @@ public class HeroStateJump : IHeroState
         hero.DisableSnap();
 
         hero.HeroAnimations.Play("HeroJump");
+
+        if (Input.IsActionJustReleased("Jump") && hero.HeroMoveLogic.Velocity.y < CutJumpThreshold)
+        {
+            hero.HeroMoveLogic.Velocity.y = JumpForceAfterJumpCutShort;
+            return hero.StateFall;
+        }
 
         if (!hero.IsOnFloor())
         {
